f607f3aa94b351ce5cfb843e1993f8ffd0ba5918
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-09-06 19:48:04 -0400

    removed unused components

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-    plugins: [
-        `gatsby-plugin-typescript`,
+  plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,

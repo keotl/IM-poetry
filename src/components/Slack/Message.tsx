9a896989a4a5efe86e19336d1a5367f90136e2cc
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-09-08 20:47:07 -0400

    basic message formatting

--- src/components/Slack/Message.tsx
+++ src/components/Slack/Message.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Message } from "../../chat/Types";
+import styled from "styled-components";
 
 type MessageComponentProps = {
   message: Message;
@@ -7,11 +8,36 @@ type MessageComponentProps = {
 
 export function MessageComponent(props: MessageComponentProps) {
   return (
-    <div>
-      {props.message.text}
-      by {props.message.user}
-      {props.message.timestamp}
-    </div>
+    <Container>
+      <Avatar src="https://ca.slack-edge.com/TCPTHK869-USLACKBOT-sv41d8cd98f0-48" />
+      <TextContent>
+        <UserName>{props.message.user}</UserName>
+        <TextContent>{props.message.text}</TextContent>
+      </TextContent>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Avatar = styled.img`
+  width: 28px;
+  height: 28px;
+`;
+
+const TextContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserName = styled.div`
+  font-weight: bold;
+`;
+
+const Content = styled.div`
+  flex: 1;
+`;

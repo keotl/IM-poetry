bb0de9e1eaf70e73ff5666a2bc9954ea9961fcbe
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-09-11 06:44:24 -0400

    slack emoji regex

--- src/components/Slack/SlackMessageRenderer.tsx
+++ src/components/Slack/SlackMessageRenderer.tsx
@@ -4,7 +4,10 @@ import React, { Fragment } from "react";
 type SlackMessageRendererProps = {
   message: Message;
 };
+const emojiRe = /:(\S+):/;
 
 export function SlackMessageRenderer(props: SlackMessageRendererProps) {
+  const matches = emojiRe.exec(props.message.text);
+
   return <Fragment>{props.message.text}</Fragment>;
 }

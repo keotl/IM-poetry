25d0b42f94cc9d69ed3260552bc6c68ea0724ba1
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-09-13 21:30:45 -0400

    recursive rich text rendering

--- src/components/Slack/SlackMessageRenderer.tsx
+++ src/components/Slack/SlackMessageRenderer.tsx
@@ -4,71 +4,103 @@ import { EmojiStoreContext, EmojiStore } from "../EmojiStore";
 import styled from "styled-components";
 import { UserStore, UserStoreContext } from "../UserStore";
 const Entities = require("html-entities").XmlEntities;
-
+const entities = new Entities();
 type SlackMessageRendererProps = {
   message: Message;
 };
-const emojiRe = /:(\S+):/;
-
-const entities = new Entities();
 
 export function SlackMessageRenderer(props: SlackMessageRendererProps) {
-    const emojiStore = useContext(EmojiStoreContext);
-    const userStore = useContext(UserStoreContext);
-
-  const rendered = [];
-  let matches = emojiRe.exec(props.message.text);
-  let message = props.message.text;
-  while (matches) {
-    const [outerText, innerText] = matches;
-      rendered.push(renderText(message.slice(0, matches.index), userStore));
-    const emoji = emojiStore.getEmoji(innerText);
-    if (emoji) {
-      rendered.push(
-        <RenderedEmoji src={emojiStore.getEmoji(innerText).image_url} />
-      );
-    } else {
-      rendered.push(<div> EMOJI {innerText} </div>);
-    }
-
-    message = message.slice(matches.index + outerText.length, message.length);
-    matches = emojiRe.exec(message);
-  }
-    rendered.push(<span>{renderText(message, userStore)} </span>);
-
-  return <Fragment>{rendered}</Fragment>;
+  const emojiStore = useContext(EmojiStoreContext);
+  const userStore = useContext(UserStoreContext);
+  return renderRichText(props.message.text, userStore, emojiStore);
 }
 
-function renderText(text: string, userStore: UserStore) {
-    return <span>{renderUserName(entities.decode(text), userStore)}</span>;
-}
-
+const emojiRe = /:(\S+):/;
 const userMentionRe = /<@(\S+)>/;
+const boldTextRe = /\*(.+)\*/;
+const strikethroughTextRe = /\~.+\~/;
 
-function renderUserName(text: string, userStore: UserStore): JSX.Element[] {
-  const rendered = [];
-  let matches = userMentionRe.exec(text);
-  let message = text;
-  while (matches) {
-    const [outerText, innerText] = matches;
-      rendered.push(<span>{message.slice(0, matches.index)}</span>);
+function renderRichText(
+  text: string,
+  userStore: UserStore,
+  emojiStore: EmojiStore
+): JSX.Element[] {
+  const userMentionMatches = userMentionRe.exec(text);
+  if (userMentionMatches) {
+    const [outerText, innerText] = userMentionMatches;
     const user = userStore.getUser(innerText);
     if (user) {
-      rendered.push(<UserMention>@{user.name} </UserMention>);
-    } else {
-      rendered.push(<span>{innerText}</span>);
+      return [
+        ...renderRichText(
+          text.slice(0, userMentionMatches.index),
+          userStore,
+          emojiStore
+        ),
+        <UserMention>@{user.name} </UserMention>,
+        ...renderRichText(
+          text.slice(userMentionMatches.index + outerText.length, text.length),
+          userStore,
+          emojiStore
+        ),
+      ];
     }
-
-    message = message.slice(matches.index + outerText.length, message.length);
-    matches = userMentionRe.exec(message);
   }
-  rendered.push(<span>{message} </span>);
-  return rendered;
-}
 
-// function renderRichText(text: string, userStore: UserStore, emojiStore: EmojiStore) : JSX.Element[] {
-//     if (userMentionRe.test())
-// }
+  const emojiMatches = emojiRe.exec(text);
+  if (emojiMatches) {
+    const [outerText, innerText] = emojiMatches;
+    const emoji = emojiStore.getEmoji(innerText);
+    if (emoji) {
+      return [
+        ...renderRichText(
+          text.slice(0, emojiMatches.index),
+          userStore,
+          emojiStore
+        ),
+        <RenderedEmoji src={emojiStore.getEmoji(innerText).image_url} />,
+        ...renderRichText(
+          text.slice(emojiMatches.index + outerText.length, text.length),
+          userStore,
+          emojiStore
+        ),
+      ];
+    } else {
+      [
+        ...renderRichText(
+          text.slice(0, emojiMatches.index),
+          userStore,
+          emojiStore
+        ),
+        <div> EMOJI {innerText}</div>,
+        ...renderRichText(
+          text.slice(emojiMatches.index + outerText.length, text.length),
+          userStore,
+          emojiStore
+        ),
+      ];
+    }
+  }
+
+  const boldMatches = boldTextRe.exec(text);
+  if (boldMatches) {
+    const [outerText, innerText] = boldMatches;
+    return [
+      ...renderRichText(
+        text.slice(0, boldMatches.index),
+        userStore,
+        emojiStore
+      ),
+      <b>{innerText}</b>,
+      ...renderRichText(
+        text.slice(boldMatches.index + outerText.length, text.length),
+        userStore,
+        emojiStore
+      ),
+    ];
+  }
+
+  return [<span>{text}</span>];
+}
 
 const RenderedEmoji = styled.img`
   width: 22px;
@@ -79,7 +111,6 @@ const RenderedEmoji = styled.img`
 `;
 
 const UserMention = styled.a`
-cursor: pointer;
-color: blue;
-
+  cursor: pointer;
+  color: blue;
 `;

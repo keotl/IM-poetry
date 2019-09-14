469d8f70ae6e358e8a662d53a183dc2ed1d82b2c
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-09-14 10:16:03 -0400

    removed unused dummy pages + precommit hook

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,43 +6,47 @@
 
 // You can delete this file if you're not using it
 
-require('source-map-support').install()
-require('ts-node').register()
+require("source-map-support").install();
+require("ts-node").register();
 
 const path = require(`path`);
-const fs = require('fs');
-const chatLogParseFactory = require("./src/chat/ChatLogParser").createChatLogParser;
-const SlackPage = require('./src/templates/SlackPage').SlackPage;
+const fs = require("fs");
+const chatLogParseFactory = require("./src/chat/ChatLogParser")
+  .createChatLogParser;
+const SlackPage = require("./src/templates/SlackPage").SlackPage;
 
 exports.createPages = ({ actions }) => {
-    const { createPage } = actions;
-    const conversationDump = fs.readFileSync('./content/archi-dump.json', { encoding: 'utf-8' });
-    const parser = chatLogParseFactory();
-    const messages = parser.extractMessages(conversationDump);
-    const users = parser.extractUsers(conversationDump);
-    const emojis = parser.extractEmojis(conversationDump);
+  const { createPage } = actions;
+  const conversationDump = fs.readFileSync("./content/archi-dump.json", {
+    encoding: "utf-8",
+  });
+  const parser = chatLogParseFactory();
+  const messages = parser.extractMessages(conversationDump);
+  const users = parser.extractUsers(conversationDump);
+  const emojis = parser.extractEmojis(conversationDump);
 
-    createPage({
-        path: "/slack",
-        component: path.resolve("./src/templates/SlackPage.tsx"),
-        context: {
-            messages,
-            users,
-            emojis,
-        }
-    });
+  createPage({
+    path: "/slack",
+    component: path.resolve("./src/templates/SlackPage.tsx"),
+    context: {
+      messages,
+      users,
+      emojis,
+    },
+  });
 };
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-exports.onCreateWebpackConfig = ({actions, loaders, getConfig}) => {
-    actions.setWebpackConfig({
-        // module:{
-            plugins: [
-                new CopyWebpackPlugin([
-                    {from: "node_modules/emoji-datasource-google/img/google/64", to: "static/emojis"}
-                ])
-            ]
-    // }
-                            })
-}
+exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: "node_modules/emoji-datasource-google/img/google/64",
+          to: "static/emojis",
+        },
+      ]),
+    ],
+  });
+};
